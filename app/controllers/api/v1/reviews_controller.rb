90b12a98ccb390d1f814ae170d1fcdbaf98be4bb
Paul McCarthy <phantasos42@gmail.com>
2019-08-03 10:55:03 -0500

    review titles

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::ReviewsController < ApplicationController
   def index
     page = Nokogiri::HTML(open(review_params))
-    render json: page.css('div.mainReviews')
+    reviews = page.css('div.mainReviews')
+    render json: presented_reviews(reviews)
   end
 
   private
 
   def review_params
     params.require(:url)
+  end
+
+  def presented_reviews(reviews)
+    reviews.map do |review|
+      {
+        title: review.css('p.reviewTitle').inner_html
+      }
+    end
   end
 end
