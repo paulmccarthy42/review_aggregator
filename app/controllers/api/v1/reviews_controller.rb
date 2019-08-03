e246c8403cda8afb929f4e7709da157866df75cd
Paul McCarthy <phantasos42@gmail.com>
2019-08-03 11:02:40 -0500

    scraper mvp, with all required data

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -14,7 +14,11 @@ class Api::V1::ReviewsController < ApplicationController
   def presented_reviews(reviews)
     reviews.map do |review|
       {
-        title: review.css('p.reviewTitle').inner_html
+        title: review.css('p.reviewTitle').inner_html,
+        content: review.css('p.reviewText').inner_html,
+        author: review.css('p.consumerName').children.first.to_s.strip,
+        stars: review.css('div.numRec').inner_html,
+        date: review.css('p.consumerReviewDate').inner_html
       }
     end
   end
