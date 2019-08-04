e54cc00e9d554d7b9f76b01198b68606d0b5d2eb
Paul McCarthy <phantasos42@gmail.com>
2019-08-04 16:56:26 -0500

    more review specific data

--- app/controllers/api/v1/helpers/reviews.rb
+++ app/controllers/api/v1/helpers/reviews.rb
@@ -1,16 +1,4 @@
 module API::V1::Helpers::Reviews
-  def review_payload(reviews)
-    reviews.map do |review|
-      {
-        title: review.css('p.reviewTitle').inner_html,
-        content: review.css('p.reviewText').inner_html,
-        author: review.css('p.consumerName').children.first.to_s.strip,
-        stars: to_int(review.css('div.numRec').inner_html),
-        date: review.css('p.consumerReviewDate').inner_html.sub('Reviewed in ', '')
-      }
-    end
-  end
-
   def well_formed_request?
     url_sections = params[:base_url].split('/')
     url_sections.count == 7 &&
@@ -22,9 +10,30 @@ module API::V1::Helpers::Reviews
     !Nokogiri::HTML(open(url)).css('section.lenderReviews').empty?
   end
 
-  private
+  def review_payload(reviews)
+    reviews.map do |review|
+      {
+        title: review.css('p.reviewTitle').inner_html,
+        content: review.css('p.reviewText').inner_html,
+        author: review.css('p.consumerName').children.first.to_s.strip,
+        stars: to_int(review.css('div.numRec').inner_html),
+        recommended: review.css('div.lenderRec').present?,
+        date: review.css('p.consumerReviewDate').inner_html.sub('Reviewed in ', ''),
+        review_points: review_points(review)
+      }
+    end
+  end
 
   def to_int(stars)
     stars[/\d/].to_i
+  end
+
+  # these "Review points" appear to vary between lending categories, which is why I'm making
+  # this section of the payload more flexible. If this data were variable enough to justify it,
+  # I'd consider storing this piece as a json in any database store.
+  def review_points(review)
+    review.css('div.reviewPoints > div > ul > li').reduce({}) do |json, point|
+      json.merge({point.css('p').inner_html => point.css('div').inner_html})
+    end
   end
 end
--- app/controllers/api/v1/reviews.rb
+++ app/controllers/api/v1/reviews.rb
@@ -1,15 +1,16 @@
 # To do:
 # 3. Investigate other available data
 # 4. Refactor
-#   a. CORS?
-# 5. Test
-# 6. Documentation
+# 5. CORS?
+# 6. Test
+# 7. Documentation
 
 module API
   module V1
     class Reviews < Grape::API
       BAD_FORMAT_ERROR = 'URL must match following format to properly return reviews: "https://www.lendingtree.com/reviews/:credit_type/:company_slug/:company_id"'.freeze
       NO_REVIEWS_ERROR = 'No reviews were found at this URL. Please visit URL manually to confirm that lender exists and page is valid'.freeze
+      INTERNAL_SERVER_ERROR = 'Internal server error encountered processing request'.freeze
 
       helpers API::V1::Helpers::Reviews
       version 'v1', using: :path
@@ -41,7 +42,7 @@ module API
 
             review_payload(reviews)
           rescue => e
-            error!({errors: e}, 500)
+            error!({error: INTERNAL_SERVER_ERROR, details: e}, 500)
           end
         end
       end
