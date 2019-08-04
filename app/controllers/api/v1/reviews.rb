4416f898d08ad2da92dde9a525b4cf1dd108ed10
Paul McCarthy <phantasos42@gmail.com>
2019-08-04 15:40:49 -0500

    page looping

--- app/controllers/api/v1/reviews.rb
+++ app/controllers/api/v1/reviews.rb
@@ -1,12 +1,12 @@
 # To do:
-# 1. Nokogiri will pull all reviews, even those hidden below the fold. Do I want to loop over pages?
 # 2. Confirm this works for different types of lender (business, mortgage, etc.)
 #   since they're given a unique url path
 # 3. Investigate other available data
 # 4. Refactor
-#   a. Grape
+#   a. CORS?
 # 5. Test
 # 6. Documentation
+# 7. Error handling
 
 module API
   module V1
@@ -17,16 +17,23 @@ module API
       prefix :api
 
       resource :reviews do
-        desc 'Return reviews at a url'
+        desc 'Returns reviews from Lending Tree, based on url for reviews. User can also request multiple pages of reviews through page_count param'
         params do
           requires :base_url, type: String
           optional :page_count, type: Integer
         end
         get do
+          page_count = params[:page_count] || 1
           reviews = []
-          url = "#{params[:base_url]}"
-          page = Nokogiri::HTML(open(url))
-          reviews += page.css('div.mainReviews')
+          i = 1
+
+          while i <= page_count
+            url = "#{params[:base_url]}?pid=#{i}"
+            page = Nokogiri::HTML(open(url))
+            reviews += page.css('div.mainReviews')
+            i += 1
+          end
+
           review_payload(reviews)
         end
       end
